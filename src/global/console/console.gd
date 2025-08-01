a578bb5eeef4928deb4535badd021c8e791e1519
GibberishDev <GibberishDevelopment@gmail.com>
2025-08-01 20:36:35 +0300

    initial

--- src/global/console/console.gd
+++ src/global/console/console.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var output_node : RichTextLabel = null
 var command_list : Dictionary = {} #TODO: move to console class
 
 class Command:
@@ -10,34 +11,55 @@ class Command:
 	var arguments_number      : int ##Number of argumanets needed for command. 0 means no arguments needed, -1 means all arguments after the command is entered are passed to callable, any other positive number means exact amopunt of arguments must be present. any more will be dropped
 	var description           : String ##Text description of command which will be shown to user when using with help command
 
-	func _init(command_name: StringName, callable: Callable, operator_status_needed: bool, is_cheat: bool, arguments_number: int = 0, description: String = "NO DESCRIPTION PROVIDED") -> void:
-		self.command_name                   = command_name
-		self.callable               = callable
-		self.operator_status_needed = operator_status_needed
-		self.is_cheat               = is_cheat
-		self.arguments_number       = arguments_number
-		self.description            = description
-
+	func _init(_command_name: StringName, _callable: Callable, _operator_status_needed: bool, _is_cheat: bool, _arguments_number: int = 0, _description: String = "NO DESCRIPTION PROVIDED") -> void:
+		self.command_name           = _command_name
+		self.callable               = _callable
+		self.operator_status_needed = _operator_status_needed
+		self.is_cheat               = _is_cheat
+		self.arguments_number       = _arguments_number
+		self.description            = _description
 
 func add_command(command_name: StringName, callable: Callable, operator_status_needed: bool, is_cheat: bool, arguments_number: int = 0, description: String = "NO DESCRIPTION PROVIDED") -> Command:
 	var new_command = Command.new(command_name, callable, operator_status_needed, is_cheat, arguments_number, description)
 	self.command_list[command_name] = new_command
 	return new_command
 
+func get_all_commands() -> String:
+	var output_string : String = ""
+	for i in self.command_list:
+		output_string += ("\n[color=gold]"
+		+ str(i) + "[/color]: " + self.command_list[i].description
+		)
+	return output_string
+
+
 func process_input(input_string: String) -> String:
 	var commands_array : Array[String] = split_commands_input(input_string) #Split commands string into an array of commands
-	
-	for i in range(split_commands.size()):
-		var command_input = split_commands[i].get_string().strip_edges()
-		var regex_comand = RegEx.new()
-		regex_comand.compile("^[a-zA-Z0-9_-]*")
-		var command_name = regex_comand.search(command_input).get_string()
-		if (self.command_list.get(StringName(command_name)) == null):
-			return ("[color=pink]command not found: [/color][color=red][b]" + StringName(command_name) + "[/b][/color]")
-		var input_arguments = command_input.right(command_name.length() * -1)
-		self.command_list[StringName(command_name)].callable.call(process_arguments(input_arguments))
+	process_commands(commands_array)
 	return ""
 
+
+func split_commands_input(input_string: String) -> Array[String]:
+	var regex_selector = RegEx.new()
+	regex_selector.compile("(\"(.*?)\"|[^;])*")
+	var regex_matches : Array[RegExMatch] = regex_selector.search_all(input_string)
+	var output : Array[String] = []
+	for i in (regex_matches.size()):
+		var match = regex_matches[i].get_string().strip_edges()
+		if match != "":
+			output.append(match)
+	return output
+
+func process_commands(commands_array: Array[String]) -> void:
+	var regex_selector = RegEx.new()
+	regex_selector.compile("^[a-zA-Z0-9_-]*")
+	for i in range(commands_array.size()):
+		var command_name = regex_selector.search(commands_array[i]).get_string()
+		if !self.command_list.has(command_name):
+			send_output_message("[color=pink]command not found: [/color][color=red][b]" + StringName(command_name) + "[/b][/color]")
+		else:
+			var input_arguments = commands_array[i].right(command_name.length() * -1)
+			self.command_list[StringName(command_name)].callable.call(process_arguments(input_arguments))
 
 func process_arguments(input_arguments: String) -> Array:
 	var regex_expression =  RegEx.new()
@@ -49,25 +71,9 @@ func process_arguments(input_arguments: String) -> Array:
 		if match != "":
 			arguments_array.append(match)
 	return arguments_array
-	
-		
-func get_all_commands() -> String:
-	var output_string : String = ""
-	for i in self.command_list:
-		output_string += ("\n[color=gold]"
-		+ str(i) + "[/color]: " + self.command_list[i].description
-		)
-	return output_string
 
+func send_output_message(message: String) -> void:
+	if output_node != null:
+		output_node.append_text("\n] " + message)
+	print_rich(message)
 
-##
-## sajkdasjkd
-##
-func split_commands_input(input_string: String) -> Array[String]:
-	var regex_selector = RegEx.new()
-	regex_selector.compile("(?:^|(?<=;)).+?(?:$|(?=;))\\s*(?!.*?\")")
-	var regex_matches : Array[RegExMatch] = regex_selector.search_all(input_string)
-	var output : Array[String] = []
-	for i in (regex_matches.size()):
-		output.append(regex_matches[i].get_string())
-	return output
--- src/server/dedicated_server_gui.gd
+++ src/server/dedicated_server_gui.gd
@@ -7,11 +7,14 @@ var output : RichTextLabel = get_node("VBoxContainer/HBoxContainer/VBoxContainer
 
 
 func _ready() -> void:
+	Console.output_node = output
 	output.focus_mode = Control.FOCUS_NONE
 	Console.add_command("help", help_command_callable, false, false, -1,
-	"Show usage of provided command or if none provided print full list of registered commands.\n	Syntax: help <command name>\n	Is cheat: false\n	Is admin only: false")
+	"Show usage of provided command or if none provided print full list of registered commands.\n	Syntax: help <command name>\n	Is cheat: false - Is admin only: false")
 	Console.add_command("echo", echo_command_callable, false, false, -1,
-	"Prints suppled text in console output\n	Syntax: echo <text...>\n	Is cheat: false\n	Is admin only: false")
+	"Prints suppled text in console output\n	Syntax: echo <text...>\n	Is cheat: false - Is admin only: false")
+	Console.add_command("clear", clear_command_callable, false, false, -1,
+	"Clears console\n	Syntax: clear\n	Is cheat: false - Is admin only: false")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,31 +23,40 @@ func _process(delta: float) -> void:
 
 func _on_console_input_text_submitted(new_text: String) -> void:
 	input.clear()
-	var regex_expression = RegEx.new()
-	regex_expression.compile("\\[.+?\\]")
-	var sanitized_text = regex_expression.sub(new_text, "", true).to_lower()
-	if sanitized_text == "": return
-	output.add_text("\n> " + sanitized_text)
-	var output_message = Console.process_input(sanitized_text)
+	#var regex_expression = RegEx.new()
+	#regex_expression.compile("\\[.+?\\]")
+	#var sanitized_text = regex_expression.sub(new_text, "", true).to_lower()
+	if new_text == "": return
+	output.add_text("\n> " + new_text)
+	var output_message = Console.process_input(new_text)
 	if output_message != "":
 		output.append_text("\n] "+output_message)
 	
 
 func help_command_callable(command_name_array: Array = []) -> void:
+	var output_text = ""
 	if command_name_array.size() == 0:
-		output.append_text("\n] All avaliable commands are:" + Console.get_all_commands())
+		output_text += ("All avaliable commands are:" + Console.get_all_commands())
+		Console.send_output_message(output_text)
 		return
 	for i in range(command_name_array.size()):
 		var command_name = command_name_array[i]
 		if  Console.command_list.get(command_name) == null:
-			output.append_text("\n] [color=pink]command not found: [/color][color=red][b]" + command_name + "[/b][/color]")
-			output.append_text("\n] All avaliable commands are:" + Console.get_all_commands())
-			return
+			output_text += ("\n] [color=pink]command not found: [/color][color=red][b]" + command_name + "[/b][/color]")
+			output_text += ("\n] All avaliable commands are:" + Console.get_all_commands())
+			break
 		var description = Console.command_list[command_name].description
-		output.append_text("\n] Help for [color=lime][b]" + str(command_name) + "[/b][/color] command:\n" + description)
+		output_text += ("\n] Help for [color=lime][b]" + str(command_name) + "[/b][/color] command:\n" + description)
+	Console.send_output_message(output_text)
+	
 
 func echo_command_callable(arguments_array: Array = []) -> void:
-	var output_text = "\n] "
+	var output_text = ""
 	for i in range(arguments_array.size()):
 		output_text += str(arguments_array[i]) + " "
-	output.append_text(output_text)
+	Console.send_output_message(output_text)
+
+func clear_command_callable(arguments_array: Array = []) -> void:
+	var escape = PackedByteArray([0x1b]).get_string_from_ascii()
+	output.clear()
+	printraw(escape + "[2J" + escape + "[;H")
