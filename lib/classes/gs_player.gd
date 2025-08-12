557b35b47d30c7900acf36376afb05e996a83d60
GibberishDev <GibberishDevelopment@gmail.com>
2025-08-13 01:28:13 +0300

    refactor unstuck to be run every physics frame in vain with Source engine

--- lib/classes/gs_player.gd
+++ lib/classes/gs_player.gd
@@ -2,7 +2,9 @@ class_name godsource_player_movement
 extends CharacterBody3D
 
 @export
-var upward_velocity_threshhold : float = 4.7625 #250 hammer units a second
+var upward_velocity_threshhold : float = 250 * 1.905 / 100
+@export
+var maximum_velocity_cap : float = 3500 * 1.905 / 100 #TODO: move maximum velocity to server settings
 
 var gravity_multiplier : float = 1.0
 
@@ -13,6 +15,7 @@ var current_movement_type : MOVEMENT_TYPE = MOVEMENT_TYPE.AIRBORNE
 
 var unstuck_offsets_table : PackedVector3Array = []
 var unstuck_offsets_table_id : int = 0
+var unstuck_check_timer : Timer
 
 enum MOVEMENT_TYPE {
 	AIRBORNE,
@@ -21,29 +24,23 @@ enum MOVEMENT_TYPE {
 	NOCLIP,
 }
 
-
 #region nodde references
-
 @onready
 var stuck_check_collider : ShapeCast3D = get_node("stuck_check_collider")
-
 #endregion
 
-var first_frame : bool = true
 func _ready() -> void:
 	unstuck_offsets_table = gen_unstuck_offsets_table()
 
 func _physics_process(delta: float) -> void:
 	process_movement(delta)
-	pass
 
 func process_movement(delta: float) -> void:
 	#step 1: check if stuck and try to unstuck and move to step 14
 	stuck_check_collider.force_shapecast_update()
 	if stuck_check_collider.is_colliding():
-		is_stuck = try_unstuck()
-		print(is_stuck)
-	else: #
+		try_unstuck()
+	else:
 		#step 2: check vertical velocity. Become airborne
 		if velocity.y >= upward_velocity_threshhold:
 			is_airborne = true
@@ -52,8 +49,8 @@ func process_movement(delta: float) -> void:
 		velocity.y -= (ProjectSettings.get_setting("physics/3d/default_gravity") * gravity_multiplier) / ProjectSettings.get_setting("physics/common/physics_ticks_per_second", 66) / 2.0
 		#step 5: handle jumping
 		#step 6: cap velocity
-		if velocity.length() > 66.675:
-			velocity = velocity.normalized() * 66.675 #TODO: move maximum velocity to server settings
+		if velocity.length() > maximum_velocity_cap:
+			velocity = velocity.normalized() * maximum_velocity_cap
 		#step 7: if not airborne apply friction and 0 out vertical velocity
 		if !is_airborne:
 			velocity.y = 0.0
@@ -72,53 +69,54 @@ func process_movement(delta: float) -> void:
 		if !is_airborne:
 			velocity.y = 0.0
 		#step 13:
-		if velocity.length() > 66.675:
-			velocity = velocity.normalized() * 66.675 #TODO: move maximum velocity to server settings
+		if velocity.length() > maximum_velocity_cap:
+			velocity = velocity.normalized() * maximum_velocity_cap
 	#step 14: check triggers to activate
-	#step 15:
+	#step 15: adjust collision hull
 		#change_collision_hull_size()
-	#step 16:
+	#step 16: process projectiles 
 		#process_projectiles()
-		first_frame = false
-	pass
 
-#valve unstuck algorythm goes through fixed list of player nudges. Some stuck bugs in TF2 are cause of this.
-#if this table outputed minimum value of y is -0.125 hammer units. and thats it.
-#Source does not try to nudge player more that in downwards direction.
+
+#region unstuck algorythm
+#---
+#Valve unstuck algorythm goes through fixed list of player nudges. Some stuck bugs in TF2 are caused by this.
+#If this table outputed minimum value of y is -0.125 hammer units. And thats it.
+#Source engine does not try to nudge player more that in downwards direction.
 #For more human frienly explanation check out Shounic video here: https://www.youtube.com/watch?v=PEhY4vE6krE
+#--- GibbDev 12AUG2028
 
 func gen_unstuck_offsets_table() -> PackedVector3Array:
-	
+	var x: float
+	var y: float
+	var z: float
 	var table : PackedVector3Array = []
-	#Little Moves
-	var little_move : float = 0.125
-	var y : float = -little_move
-	while y <= little_move:
+	#Little Moves --- Valve
+	y = -0.125
+	while y <= 0.125:
 		table.append(Vector3(0,y,0))
-		y += little_move
-	var x : float = -little_move
-	while x <= little_move:
+		y += 0.125
+	x = -0.125
+	while x <= 0.125:
 		table.append(Vector3(x,0,0))
-		x += little_move
-	var z : float = -little_move
-	while z <= little_move:
+		x += 0.125
+	z = -0.125
+	while z <= 0.125:
 		table.append(Vector3(0,0,z))
-		z += little_move
-	#Remaining multi axis nudges.
-	y = -little_move
-	while y <= little_move:
-		x = -little_move
-		while x <= little_move:
-			z = -little_move
-			while z <= little_move:
+		z += 0.125
+	#Remaining multi axis nudges. --- Valve
+	y = -0.125
+	while y <= 0.125:
+		x = -0.125
+		while x <= 0.125:
+			z = -0.125
+			while z <= 0.125:
 				table.append(Vector3(x, y, z))
 				z += 0.25
 			x += 0.25
 		y += 0.25
-	
-	# Big Moves.
+	# Big Moves. --- Valve
 	var yi : PackedFloat32Array = [0.0, 1.0, 6.0]
-	
 	for i : float in yi:
 		y = i
 		table.append(Vector3(0, y, 0))
@@ -130,7 +128,6 @@ func gen_unstuck_offsets_table() -> PackedVector3Array:
 	while z <= 2.0:
 		table.append(Vector3(0, 0, z))
 		z += 2.0
-	
 	for i : float in yi:
 		y = i
 		x = -2.0
@@ -140,29 +137,33 @@ func gen_unstuck_offsets_table() -> PackedVector3Array:
 				table.append(Vector3(x, y, z))
 				z += 2.0
 			x += 2.0
-
-	#convert hammer units to meters:
+	#convert hammer units to meters: --- GibbDev
 	for i : int in range(table.size()):
 		table[i] *= 1.905 / 100
-
 	return table
 
+#pull offset off the unstuck table using player offset
 func get_unstuck_offset() -> Vector3:
 	var unstuck_offset : Vector3 = unstuck_offsets_table[unstuck_offsets_table_id % 53]
 	unstuck_offsets_table_id += 1
 	return unstuck_offset
 
-func reset_unstuck_offsets_table_id() -> void:
+#iterate through unstuck offsets table and move player in first empty space
+#HACK:	Iterates every physics frame. If it becomes a problem with physics perfomance switch to iterating at intervals.
+#		Valve decided to iterate no sooner than 0.05 seconds from last iteration. Roughly every 4 physics frames at 66 pfps 
+func try_unstuck() -> void:
+	var unstuck_offset : Vector3
+	for i: int in range(53):
+		unstuck_offset = get_unstuck_offset()
+		stuck_check_collider.position = unstuck_offset
+		stuck_check_collider.force_shapecast_update()
+		if !stuck_check_collider.is_colliding():
+			global_position += unstuck_offset
+			is_stuck = true
+			break
 	unstuck_offsets_table_id = 0
-
-func try_unstuck() -> bool:
-	var unstuck_offset : Vector3 = get_unstuck_offset()
-	stuck_check_collider.position = unstuck_offset
-	stuck_check_collider.force_shapecast_update()
-	if stuck_check_collider.is_colliding():
-		stuck_check_collider.position = Vector3.ZERO
-		return false
+	is_stuck = false
 	stuck_check_collider.position = Vector3.ZERO
-	global_position += unstuck_offset
-	reset_unstuck_offsets_table_id()
-	return true
+
+
+#endregion
